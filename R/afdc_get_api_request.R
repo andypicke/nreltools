12bbe5559dfab96f6b2e27c1917f645332e60cbc
Andy <andypicke@gmail.com>
2024-05-21 15:57:58 -0600

    pass api key as header instead of pasting to end of url

--- R/afdc_get_api_request.R
+++ R/afdc_get_api_request.R
@@ -1,11 +1,12 @@
 #' Make API GET request and return data
 #' @param query_url URL for the API query
+#' @param api_key API key (default is Sys.getenv("NREL_DEVELOPER_API_KEY"))
 #' @returns df: data from the API response
 #' @export
 
-afdc_get_api_request <- function(query_url){
+afdc_get_api_request <- function(query_url, api_key = Sys.getenv("NREL_DEVELOPER_API_KEY")){
 
-  resp <- httr::GET(url = query_url, httr::user_agent("nreltools"))
+  resp <- httr::GET(url = query_url, httr::add_headers('X-Api-Key' = api_key), httr::user_agent("nreltools: https://github.com/andypicke/nreltools"))
 
   if (resp$status_code != 200) {
     stop(paste("Warning, API call returned error code", resp$status_code))
