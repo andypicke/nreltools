ddc66fd5ea5acc3aa94b3435844c42e8312bc582
Andy <andypicke@gmail.com>
2024-05-21 15:42:30 -0600

    change name of api key variable

--- R/afdc_construct_query_url.R
+++ R/afdc_construct_query_url.R
@@ -2,14 +2,14 @@
 #' @param fuel_type Fuel type, default = "ELEC"
 #' @param wh_state State to get data for
 #' @param limit Number of rows to return; default is "all"
-#' @param api_key API key (default is Sys.getenv("AFDC_KEY"))
+#' @param api_key API key (default is Sys.getenv("NREL_DEVELOPER_API_KEY"))
 #' @returns query_url URL for API request
 #' @export
 
 afdc_construct_query_url <- function(fuel_type = "ELEC",
                                      wh_state = "CO",
                                      limit = "all",
-                                     api_key = Sys.getenv("AFDC_KEY")){
+                                     api_key = Sys.getenv("NREL_DEVELOPER_API_KEY")){
 
   # base url for AFDC alternative fuel stations API
   target <- "https://developer.nrel.gov/api/alt-fuel-stations/v1"
